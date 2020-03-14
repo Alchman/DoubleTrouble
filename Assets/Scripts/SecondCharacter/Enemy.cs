bc758b87559b42d45756768c736db022b47885b3
Screwdriver1982 <60478923+Screwdriver1982@users.noreply.github.com>
2020-03-14 15:53:37 +0300

    из класса

--- Assets/Scripts/SecondCharacter/Enemy.cs
+++ Assets/Scripts/SecondCharacter/Enemy.cs
@@ -5,15 +5,39 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
+    [SerializeField] private float speed = 14f;
+    [SerializeField] private float playerdistance = 5f;
+    
     private SecondCharacter _secondCharacter;
+    private Rigidbody rb;
+    private float distance;
+    
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         _secondCharacter = FindObjectOfType<SecondCharacter>();
+        
     }
 
-    void Update()
+    void FixedUpdate()
     {
+
+        
         Vector3 target = _secondCharacter.transform.position;
-        transform.position = Vector3.MoveTowards(transform.position, target, 0.1f);
+        Vector3 enemy = transform.position;
+        Vector3 direction = (target - enemy);
+        direction.y = rb.velocity.y;
+      //  direction.y = 0;
+        rb.velocity = direction.normalized * speed;
+      distance =  Vector3.Distance(enemy, target);
+        ChekDistance();
+    }
+
+    private void ChekDistance()
+    {
+        if (distance < playerdistance)
+        {
+            rb.velocity = Vector3.zero;
+        }
     }
 }

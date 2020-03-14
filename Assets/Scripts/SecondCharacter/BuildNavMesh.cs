b99f32cec96abae8fb8cef412d38ed356d8f8d11
Screwdriver1982 <60478923+Screwdriver1982@users.noreply.github.com>
2020-03-14 18:37:45 +0300

    настрока меша

---
+++ Assets/Scripts/SecondCharacter/BuildNavMesh.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BuildNavMesh : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<NavMeshSurface>().BuildNavMesh();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

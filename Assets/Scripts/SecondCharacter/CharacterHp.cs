59e0da577a04d048633ee8f437fe8887d33f4dcd
lk <lkghost7@gmail.com>
2020-03-21 17:47:28 +0300

    CharacterHp

--- Assets/Scripts/SecondCharacter/CharacterHp.cs
+++ Assets/Scripts/SecondCharacter/CharacterHp.cs
@@ -1,14 +1,29 @@
-﻿using UnityEngine;
-
-
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterHp : MonoBehaviour
 {
-  public int health = 100;
-  // public Slider slider;
+    public float  health = 100;
+    public Slider slider;
 
-  private void Update()
-  {
-    // slider.value = health;
-  }
+    Regeneration regeneration;
+
+    /* private void Update()
+     {
+       slider.value = health;
+     }*/
+
+    private void Start()
+    {
+        regeneration = FindObjectOfType<Regeneration>();
+    }
+
+    public void RegenerationHealth()
+    {
+        health += regeneration.GetRegeneration();
+    }
+
 }

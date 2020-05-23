dec1f226178de45082adb86f20d2f3f851a750a0
vityapechyrikhin <vityapechyrikhin@gmail.com>
2020-05-23 13:10:24 +0300

    возврат камеры

--- Assets/Scripts/ChangeCamera.cs
+++ Assets/Scripts/ChangeCamera.cs
@@ -6,19 +6,38 @@ public class ChangeCamera : MonoBehaviour
 {
     public float speed;
     public GameObject camera;
+    public GameObject player;
+    Vector3 oldPosition;
+    bool moveCamera = false;
 
     void Update()
     {
 
         Vector3 pos = transform.position;
+      
         if (Input.GetKeyDown(KeyCode.F1))
         {
             Time.timeScale = 0;
             camera.SetActive(false);
+             oldPosition = transform.position;
+            moveCamera = true;
         }
-        pos.z += Input.GetAxis("Vertical") * speed * Time.unscaledDeltaTime;
-        pos.x += Input.GetAxis("Horizontal") * speed * Time.unscaledDeltaTime;
-        transform.position = pos;
+        if (moveCamera)
+        {
+            pos.z += Input.GetAxisRaw("Vertical") * speed * Time.unscaledDeltaTime;
+            pos.x += Input.GetAxisRaw("Horizontal") * speed * Time.unscaledDeltaTime;
+            transform.position = pos;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            moveCamera = false;
+            Time.timeScale = 1;
+            transform.position = oldPosition;
+            camera.SetActive(true);
+         
+           
+        }
 
 
     }

149c1a47c02f1d99a5a878830eb5e841c907edca
vityapechyrikhin <vityapechyrikhin@gmail.com>
2020-05-23 14:11:39 +0300

    свободная камера

--- Assets/Scripts/ChangeCamera.cs
+++ Assets/Scripts/ChangeCamera.cs
@@ -5,36 +5,53 @@ using UnityEngine;
 public class ChangeCamera : MonoBehaviour
 {
     public float speed;
-    public GameObject camera;
-    public GameObject player;
-    Vector3 oldPosition;
+    public Camera mainCamera;
+    public Camera camera;
+  
+    
     bool moveCamera = false;
 
+    private void Start()
+    {
+        //mainCamera.gameObject.SetActive(true);
+      
+        camera.gameObject.SetActive(false);
+    
+     
+      
+    }
     void Update()
     {
-
-        Vector3 pos = transform.position;
+       
+        
+     
       
         if (Input.GetKeyDown(KeyCode.F1))
         {
+
             Time.timeScale = 0;
-            camera.SetActive(false);
-             oldPosition = transform.position;
+            camera.transform.position = mainCamera.transform.position;
+            camera.transform.rotation = mainCamera.transform.rotation;
+            camera.orthographicSize = mainCamera.orthographicSize;
+            mainCamera.gameObject.SetActive(false);
+            camera.gameObject.SetActive(true);
             moveCamera = true;
         }
         if (moveCamera)
         {
+            Vector3 pos = camera.transform.position;
             pos.z += Input.GetAxisRaw("Vertical") * speed * Time.unscaledDeltaTime;
             pos.x += Input.GetAxisRaw("Horizontal") * speed * Time.unscaledDeltaTime;
-            transform.position = pos;
+            camera.transform.position = pos;
         }
         
         if (Input.GetKeyDown(KeyCode.F2))
         {
+            mainCamera.gameObject.SetActive(true);
+            camera.gameObject.SetActive(false);
             moveCamera = false;
             Time.timeScale = 1;
-            transform.position = oldPosition;
-            camera.SetActive(true);
+           
          
            
         }

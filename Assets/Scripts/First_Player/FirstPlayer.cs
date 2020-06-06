d7a3beadb89ea6add27c6317e7097908b1972f59
vityapechyrikhin <vityapechyrikhin@gmail.com>
2020-06-06 12:58:19 +0300

    синглтон у первого игрока, и скрипт компаса

--- Assets/Scripts/First_Player/FirstPlayer.cs
+++ Assets/Scripts/First_Player/FirstPlayer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstPlayer : MonoBehaviour
+public class FirstPlayer : GenericSingletonClass<FirstPlayer>
 {
     public LayerMask pushMask;
 

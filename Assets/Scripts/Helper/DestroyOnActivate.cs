4c285deab68acdfc52b2db97921535ee32e42ae0
Denis <Denis>
2020-08-01 14:00:30 +0300

    destroy on activate

---
+++ Assets/Scripts/Helper/DestroyOnActivate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnActivate : MonoBehaviour
+{
+    private void Awake()
+    {
+        Destroy(gameObject);
+    }
+}

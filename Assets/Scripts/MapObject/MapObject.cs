ffd1358137e31bf22b2c0fb0f5ae373590a34326
lk <lkghost7@gmail.com>
2020-08-01 13:45:36 +0300

    скрипт мэпобжект

---
+++ Assets/Scripts/MapObject/MapObject.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MapObject : MonoBehaviour{
+
+    [SerializeField][Tooltip("Время на которое отключается кинематика обьекта для пинания")] private float timeOfKinematic =1f;
+    
+    private Coroutine coroutineOfKinematic;
+    private Pushable pushable;
+    private Rigidbody rb;
+    private void Start() {
+
+        rb = GetComponent<Rigidbody>();
+        pushable = GetComponent<Pushable>();
+        pushable.PushObject += PuchControl;
+    }
+
+    private void PuchControl() {
+        // print("PuchControl");
+        if(coroutineOfKinematic != null) {
+            StopCoroutine(coroutineOfKinematic);
+        }
+            coroutineOfKinematic = StartCoroutine(OfKinematic());
+    }
+
+    IEnumerator OfKinematic() {
+        rb.isKinematic = false;
+        // print("corutin");
+        yield return new WaitForSeconds(timeOfKinematic);
+        rb.isKinematic = true;
+    }
+}
+

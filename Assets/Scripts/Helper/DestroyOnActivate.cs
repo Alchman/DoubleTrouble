6fd91582a86878a525a0988721212abfa12ea515
Denis <Denis>
2020-08-01 14:32:11 +0300

    DestroyOnActivate fix

--- Assets/Scripts/Helper/DestroyOnActivate.cs
+++ Assets/Scripts/Helper/DestroyOnActivate.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class DestroyOnActivate : MonoBehaviour
 {
-    private void Awake()
+    //    private void Awake()
+    //    {
+    //    }
+
+    private void Start()
     {
         Destroy(gameObject);
     }

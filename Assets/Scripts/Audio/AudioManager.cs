9bf8cdbf44a5f04a3cf265baa6eefaf79b93a4e0
Denis <Denis>
2020-08-22 11:22:46 +0300

    souns

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : GenericSingletonClass<AudioManager>
+{
+    [SerializeField] AudioSource effects;
+
+    public void PlaySound(AudioClip sound)
+    {
+        effects.PlayOneShot(sound);
+    }
+}
